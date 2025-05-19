11f18a56fee1ac530c3287f3316c681f441d65fb
BSSMeghana <meghana.bantubilli@gmail.com>
2025-05-20 00:14:54 +0530

    Temp rename to fix casing

---
+++ app/temp.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function Intro() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/images/eyeand.png')} style={styles.logo} />
+      <Text style={styles.title}>DRUSHTI</Text>
+
+      <TouchableOpacity style={styles.button} onPress={() => router.replace('./quiz')}>
+        <Text style={styles.buttonText}>Let's Get Started!</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#e6f0ff', justifyContent: 'center', alignItems: 'center' },
+  logo: { width: 150, height: 150, marginBottom: 18, borderRadius: 50, },
+  title: { fontSize: 32, fontWeight: 'bold', color: '#003366', marginBottom: 70 },
+  button: { backgroundColor: '#4fa9f6', paddingVertical: 12, paddingHorizontal: 38, borderRadius: 10 },
+  buttonText: { color: 'white', fontSize: 18 },
+ });
